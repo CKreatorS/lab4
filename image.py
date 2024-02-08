b45c3cab73014a99261b982478b3f38f471c1285
cmorales <caitlyn.morales424@gmail.com>
2024-02-08 09:45:05 -0500

    updates

--- image.py
+++ image.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, request, redirect, render_template
 
 app = Flask(__name__)
 
 @app.route('/imag')
 def serve_image():
     "a simple HTTP image"
-    return render_template('blob:chrome-untrusted://media-app/fa742c2c-e46a-475a-868f-02ce1c5f7b00')
+    return render_template()
 
 if __name__ == '__main__':
     app.run(host='localhost', port=8081)
